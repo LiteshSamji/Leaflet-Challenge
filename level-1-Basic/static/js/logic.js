ac7ef0ea525c7556527a8404f8b4147ec417ad9f
Litesh Samji <litesh@y7mail.com>
2021-09-02 16:36:21 +1000

    Add circles  with labels to he map

--- level-1-Basic/static/js/logic.js
+++ level-1-Basic/static/js/logic.js
@@ -16,7 +16,52 @@ var url_earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 
 // Grab data with d3
 d3.json(url_earthquakes).then(function(data) {
-   
+    // function getColor(d) {
+    //     return d >= 5 ? "rgb(240, 107, 107)" :
+    //            d >= 4 ? "rgb(240, 167, 107)" :
+    //            d >= 3 ? "rgb(243, 186, 77)" :
+    //                      d >= 2 ? "rgb(243, 219, 77)" :
+    //                      d >= 1 ? "rgb(225, 243, 77)" :
+    //                                          "rgb(183, 243, 77)";
+    //     }
+    function chooseColor(magnitude) {
+        switch(true) {
+          case magnitude > 5:
+            return "red";
+          case magnitude > 4:
+            return "orangered";
+          case magnitude > 3:
+            return "orange";
+          case magnitude > 2:
+            return "gold";
+          case magnitude > 1:
+            return "yellow";
+          default:
+            return "lightgreen";
+        }
+      }
+
+    // Grab the features data
+	var features = data.features;
+
+	for (var i = 0; i < features.length; i++) {
+		
+		//Define variable magnitudes and coordinates of the earthquakes
+		var magnitudes = features[i].properties.mag;
+		var coordinates = features[i].geometry.coordinates;
+
+		// Add circles to map
+		L.circle(
+            [coordinates[1], coordinates[0]], {
+				fillOpacity: 0.75,
+				fillColor: chooseColor(magnitudes),
+				color: "black",
+				weight: 0.5,
+				radius: magnitudes * 15000
+			}).bindPopup("<h3>" + features[i].properties.place +
+				"</h3><hr><p>" + new Date(features[i].properties.time) + 
+				'<br>' + '[' + coordinates[1] + ', ' + coordinates[0] + ']' + "</p>").addTo(myMap);
+	};	
 
 });
 
