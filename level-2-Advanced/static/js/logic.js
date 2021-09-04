89f81363000914d2aa9a7504e3c98e2fd8671c66
Litesh Samji <litesh@y7mail.com>
2021-09-04 13:13:14 +1000

    Function for creating legend

--- level-2-Advanced/static/js/logic.js
+++ level-2-Advanced/static/js/logic.js
@@ -50,6 +50,28 @@ function createMap(GeoJsonLayer, platesLayer){
     return myMap;
 }
 
+// Function for creating legend
+function createLegend(map){
+    // Create a legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            mag = [0,1, 2, 3, 4, 5]
+        div.innerHTML += "<h4>Magnitude Level</h4><hr>"
+        // loop and generate a label with a colored square for each interval
+        for (var i = 0; i < mag.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(mag[i] + 1) + '"></i> ' +
+                mag[i] + (mag[i + 1] ? '&ndash;' + mag[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(map);
+}
+
+
 // Function for creating GeoJSON layer
 function createGeoJsonLayer(data){
     var GeoJsonLayer = L.geoJson(data,{
